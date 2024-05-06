95b80c539845b160c5f79e1b9fcc74b1f8fd6db7
Shaheer Malik <22034133@student.curtin.edu.au>
2024-05-06 23:44:07 +0800

    Updated the class name

--- Maze.java
+++ Maze.java
@@ -25,7 +25,7 @@ public class Maze
             { WALL,  WALL, EMPTY, EMPTY, EMPTY, EMPTY,  WALL,  WALL, EMPTY,  END},
             { WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL, WALL},
         };
-        GridViewer.view(grid);
+        Viewer.view(grid);
         String solution;
         Scanner sc = new Scanner(System.in);
         System.out.println("Find a solution to get from ^^ to $$, using the characters " 
@@ -103,6 +103,6 @@ public class Maze
         {
             System.out.println("MESSAGE 5"); // Did not reach the end.
         }
-        GridViewer.view(grid);
+        Viewer.view(grid);
     }
 }
